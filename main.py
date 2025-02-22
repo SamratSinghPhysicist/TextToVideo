878f715201421565aaeae95bfc713fdcc2243406
Samrat Singh <101398996+SamratSinghPhysicist@users.noreply.github.com>
2025-02-22 18:35:28 +0530

    Update timing

--- main.py
+++ main.py
@@ -285,9 +285,9 @@ def process_video():
 # Schedule the process to run twice daily
 # ---------------------------
 # Schedule first upload at 2:00 PM and second at 6:00 PM
-schedule.every().day.at("18:20").do(process_video)
-schedule.every().day.at("18:35").do(process_video)
-schedule.every().day.at("19:00").do(process_video)
+schedule.every().day.at("18:40").do(process_video)
+schedule.every().day.at("18:55").do(process_video)
+schedule.every().day.at("19:30").do(process_video)
 
 print("Scheduler is running. Waiting for next scheduled time...")
 while True:
