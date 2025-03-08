909259af9a817af87dd7b0d9223169686e7cf91d
Samrat Singh <101398996+SamratSinghPhysicist@users.noreply.github.com>
2025-03-08 16:08:36 +0530

    Update topic_fetcher.py

--- topic_fetcher.py
+++ topic_fetcher.py
@@ -5,7 +5,6 @@ import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
 import json
